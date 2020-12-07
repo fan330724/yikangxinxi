2f8a327c5a2a2f9233596be659ef9283b97ae296
zhengff <873274951@qq.com>
2020-12-07 16:24:03 +0800

    12月7日

--- pages/repair/repair.js
+++ pages/repair/repair.js
@@ -300,7 +300,7 @@ Page({
     wx.chooseImage({
       count: 1, // 最多可以选择的图片张数，默认9
       sizeType: ['original', 'compressed'], // original 原图，compressed 压缩图，默认二者都有
-      sourceType: ['album'], // album 从相册选图，camera 使用相机，默认二者都有
+      sourceType: ['album', 'camera'], // album 从相册选图，camera 使用相机，默认二者都有
       success: function (res) {
         // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
         var tempFilePaths = res.tempFilePaths;
